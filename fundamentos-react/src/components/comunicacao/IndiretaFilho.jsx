fe5eb9f66253af3e9ad119f699016b81133bb2d1
Miguel Rodrigues <migvidro@gmail.com>
2021-04-15 22:10:11 -0300

    useState and controlled components

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const IndiretaFilho = (props) => {
+  const min = 50;
+  const max = 70;
+  const gerarIdade = () => parseInt(Math.random() * (max - min)) + min;
+  const gerarNerd = () => Math.random() > 0.5;
   return (
     <div>
       <div>Filho</div>
-      <button onClick={(e) => props.quandoClicar("Joao", 53, true)}>
+      <button onClick={(e) => props.quandoClicar("Joao", gerarIdade(), gerarNerd())}>
         Fornecer Informacoes
       </button>
     </div>
