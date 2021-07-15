ed5c12204b8d1c0dc9492d83e1f0ad03c01c942b
Miguel Rodrigues <migvidro@gmail.com>
2021-07-15 10:04:27 -0300

    Exact parameter and not found route

--- navigation/src/components/layout/Content.jsx
+++ navigation/src/components/layout/Content.jsx
@@ -5,6 +5,7 @@ import "./Content.css";
 import Home from "../../views/examples/Home";
 import About from "../../views/examples/About";
 import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = (props) => {
   return (
@@ -16,8 +17,11 @@ const Content = (props) => {
         <Route path="/param/:id">
           <Param />
         </Route>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </main>
