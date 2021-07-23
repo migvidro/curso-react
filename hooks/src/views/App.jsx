2e243981121bab100965714a5c7fe7644ebc79f0
Miguel Rodrigues <migvidro@gmail.com>
2021-07-23 15:53:04 -0300

    useContext and Context API

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -1,20 +1,28 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Menu from '../components/layout/Menu'
-import Content from '../components/layout/Content'
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
 
-const App = props => {
+import { DataContext, data } from "../data/DataContext";
+import Store from "../data/Store";
 
-    return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
-    )
-}
+const App = (props) => {
+  const [state, setState] = useState(data);
 
-export default App
+  return (
+    <Store>
+      <DataContext.Provider value={{ state, setState }}>
+        <div className="App">
+          <Router>
+            <Menu />
+            <Content />
+          </Router>
+        </div>
+      </DataContext.Provider>
+    </Store>
+  );
+};
+
+export default App;
