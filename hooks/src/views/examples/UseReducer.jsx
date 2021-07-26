bc1306db9d61650b6b0a6e52b3c3869f10feeecf
Miguel Rodrigues <migvidro@gmail.com>
2021-07-26 00:13:56 -0300

    useReducer exercise part I

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useReducer } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    number: 0,
+  };
 
-export default UseReducer
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "numberAdd2":
+        return { ...state, number: state.number + 2 };
+      case "login":
+        return { ...state, user: { name: action.payload } };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem Usuario</span>
+        )}
+
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "login", payload: "Miguel" })}
+          >
+            Login
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAdd2" })}
+          >
+            +2
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducer;
